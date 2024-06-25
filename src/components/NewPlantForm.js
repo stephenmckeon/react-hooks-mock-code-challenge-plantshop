7942399a9f81d664db675fac08297adc65cf344f
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-06-25 14:01:18 -0400

    Post form data

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react"
 
 function NewPlantForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: 0,
+  })
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const sanitizeFormData = () => ({
+    ...formData,
+    price: Number(formData.price),
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sanitizeFormData()),
+    })
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="Plant name"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="image"
+          placeholder="Image URL"
+        />
+        <input
+          onChange={handleChange}
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
-  );
+  )
 }
 
-export default NewPlantForm;
+export default NewPlantForm
